c818825e1e35071b4e8997d0fb06fcafed34b1f4
Cole Stiefel <cstiefel@ravenscroft.org>
2019-05-02 11:59:55 -0400

    work

--- balldrop.py
+++ balldrop.py
@@ -3,9 +3,13 @@ from pygame.locals import *
 from ball import ball
 from hoop import hoop
 
+WHITE = (255,255,255)
 DISPLAYSURF = pygame.display.set_mode((300,400), 0, 32)
 FPS = 10
 fpsClock = pygame.time.Clock()
+
+HOOP = pygame.image.load('Basketball Hoop.png')
+HOOP = pygame.transform.scale(HOOP, (45,45))
 
 pygame.display.set_caption("Ball Drop")
 
@@ -15,20 +19,34 @@ hoopob = hoop(0)
 direction = 'right'
 
 def hoop_move():
-    if direction == 'right'
-        if hoopob.return_x() != 300:
-            hoopob.move_right()
-        if hoopob.return_x() == 300:
-            hoopob.move_left()
+    if direction == 'right':
+        hoopob.move_right()
+    if direction == 'left':
+        hoopob.move_left()
 
-    if direction == 'left'
+def switch_direction(direction):
+    if direction == 'right' and hoopob.return_x() == 255:
+        direction = 'left'
+    if direction == 'left' and hoopob.return_x() == 0:
+        direction = 'right'
+    return direction
 
-def main():
+def update_hoop():
+    DISPLAYSURF.blit(HOOP, (hoopob.rect.x,355,45,45))
+
+while True:
+
+    DISPLAYSURF.fill(WHITE)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    hoop_move()
+    update_hoop()
+
+    direction = switch_direction(direction)
 
     pygame.display.update()
     fpsClock.tick(FPS)
--- hoop.py
+++ hoop.py
@@ -1,22 +1,19 @@
 import pygame, sys, time, random
 from pygame.locals import *
 
-hoop = pygame.image.load('Basketball Hoop.png')
-hoop = pygame.transform.scale(hoop, (35,35))
-
 class hoop(pygame.sprite.Sprite):
 
     def __init__(self,x):
 
         super().__init__()
         self.x = x
-        self.rect = pygame.Rect(self.x,365,35,1)
+        self.rect = pygame.Rect(self.x,355,45,1)
 
     def return_x(self):
-        return self.x
+        return self.rect.x
 
     def move_right(self):
         self.rect.x += 5
 
     def move_left(self):
-        self.rect.y += 5
+        self.rect.x -= 5
