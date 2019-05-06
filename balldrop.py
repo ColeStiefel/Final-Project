25703f40ef3d25ec5ba5cb7fbe18918edaca6bf4
Cole Stiefel <cstiefel@ravenscroft.org>
2019-05-06 13:31:10 -0400

    yeet

--- balldrop.py
+++ balldrop.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 from ball import ball
 from hoop import hoop
 
+pygame.init()
+
 WHITE = (255,255,255)
 DISPLAYSURF = pygame.display.set_mode((300,400), 0, 32)
 FPS = 10
@@ -17,6 +19,20 @@ ballob = ball(135,0)
 hoopob = hoop(0)
 
 direction = 'right'
+
+opener = 'true'
+
+def opening_screen():
+    BASICFONT_large = pygame.font.Font('freesansbold.ttf', 60)
+    txt = BASICFONT_large.render('Ball Dropper',1,WHITE)
+    ball_dropper_rect = DISPLAYSURF.get_rect()
+    ball_dropper_rect.center = (212,250)
+    DISPLAYSURF.blit(txt, ball_dropper_rect)
+
+def switch_opener(opener):
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #use pygame.mouse.get_pos
 
 def hoop_move():
     if direction == 'right':
@@ -35,8 +51,10 @@ def update_hoop():
     DISPLAYSURF.blit(HOOP, (hoopob.rect.x,355,45,45))
 
 while True:
-
     DISPLAYSURF.fill(WHITE)
+
+    if opener() == 'true'
+        opening_screen()
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -50,5 +68,3 @@ while True:
 
     pygame.display.update()
     fpsClock.tick(FPS)
-
-main()
